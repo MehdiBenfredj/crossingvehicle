3ad816d97482279899f9af80410431d28b00888c
rosen902 <corentin.ehlinger@gmail.com>
2023-11-19 17:55:13 +0100

    Test application du chromosome

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import libsumo as traci
 from service import Service
 
 # Créer un Service
@@ -6,4 +7,8 @@ srv = Service("sumofiles")
 # Générer le rou file
 srv.generate_rou_file()
 
-# Lancer sumo manuellement
+# Lancer sumo avec python
+traci.start(["sumo-gui", "-c", "sumofiles/config.sumocfg"])
+
+srv.apply_chromosom("J1", (3,3,3,3))
+print(traci.trafficlight.getCompleteRedYellowGreenDefinition("J1"))
