3da9637816fb733470ddd4013eff2216851e81c4
rosen902 <corentin.ehlinger@gmail.com>
2023-11-23 00:32:37 +0100

    Genetic algorithm functions def

--- algcfg.py
+++ algcfg.py
@@ -2,11 +2,18 @@ from dotenv import dotenv_values
 
 class GeneticConfig():
     def __init__(self) -> None:
-        self.gui = "hello"
-        self.sumo_folder = "world"
-        self.sumo_cfg_file = 6
-        self.min_phase_time = 6
-        self.max_cycle_time = 6
+        self.gui = None
+        self.sumo_folder = None
+        self.sumo_cfg_file = None
+        self.iterations = None
+        self.intersection_id = None
+        self.min_phase_time = None
+        self.max_cycle_time = None
+        self.parents_number = None
+        self.children_number = None
+        self.crossing_points = None
+        self.mutation_proba = None
+        self.initial_pop_size = None
         # other params...
 
 
@@ -15,19 +22,26 @@ class GeneticConfig():
     
     
     def print_undefined_items(self):
-        print("Unable to run algorithm (Bad config) :")
+        print("Unable to run algorithm (Bad configuration) :")
         for k, value in vars(self).items():
             if value == None:
-                print("- {} undefined")
+                print("- {} undefined".format(k))
                 
 
     def load_from_env_file(self, file : str = "env"):
         try:
             values = dotenv_values()
-            self.gui = values["GUI"].lower() in ["true", "t", "1", "yes"]
+            self.gui = values["GUI"].lower() in ["true", "t", "1", "yes", "y"]
             self.sumo_folder = values["SUMO_FOLDER"]
             self.sumo_cfg_file = values["SUMO_CFG_FILE"]
+            self.iterations = values["ITERATIONS"]
+            self.intersection_id = values["INTERSECTION_ID"]
             self.min_phase_time = values["MIN_PHASE_TIME"]
             self.max_cycle_time = values["MAX_CYCLE_TIME"]
+            self.parents_number = values["PARENTS_NUMBER"]
+            self.children_number = values["CHILDREN_NUMBER"]
+            self.crossing_points = values["CROSSING_POINTS"]
+            self.mutation_proba = values["MUTATION_PROBA"]
+            self.initial_pop_size = values["INITIAL_POP_SIZE"]
         except:
             pass
--- genetic.py
+++ genetic.py
@@ -1,49 +1,45 @@
 from service import Service
+from algcfg import GeneticConfig
 import random
 import libsumo as traci
 import os
 
 class GeneticAlgorithm():
 
-    def __init__(self, gui_active : bool, sumo_folder : str, sumo_cfg_file : str) -> None:
-        self.gui = gui_active
-        self.sumo_folder = sumo_folder
-        self.sumo_cfg = sumo_cfg_file
-
-        self.population = []
-        self.fitness = []
+    def __init__(self, config : GeneticConfig) -> None:
         
-        self.srv = Service(sumo_folder)
+        self.params = config
+        self.population_and_fitness = []
+        self.srv = Service(config.sumo_folder)
 
 
-    def _sanitize_chromosom(self, min_phase_time : int, max_cycle_time : int, chromosom : list) -> list:
+    def _sanitize_chromosom(self, chromosom : list) -> list:
 
         indexes = [i for i in range(len(chromosom))]
-        while sum(chromosom) > max_cycle_time:
+        while sum(chromosom) > self.params.max_cycle_time:
             decrement_index = random.choices(indexes, weights=chromosom, k=1)[0]
-            if chromosom[decrement_index] > min_phase_time:
+            if chromosom[decrement_index] > self.params.min_phase_time:
                 chromosom[decrement_index] -= 1
 
         return chromosom
 
 
-    def generate_initial_pop(self, max_cycle_time : int, size : int):
+    def _generate_initial_pop(self) -> list[list[float]]:
         pass
-
+        
 
     
 
     def _compute_fitness(self, chromosom : list) -> float:
         
         time_per_vehicle = {}
-         
-        conf_file_path = os.path.join(self.sumo_folder, self.sumo_cfg)
-        if self.gui:
+        conf_file_path = os.path.join(self.params.sumo_folder, self.params.sumo_cfg_file)
+        if self.params.gui:
             traci.start(["sumo-gui", "-c", conf_file_path])
         else:
             traci.start(["sumo", "-c", conf_file_path])
 
-        self.srv.apply_chromosom("J0", chromosom)
+        self.srv.apply_chromosom(self.params.intersection_id, chromosom)
         while traci.simulation.getMinExpectedNumber() > 0:
             
             vehicles_in_aera = self.srv.step()
@@ -60,11 +56,16 @@ class GeneticAlgorithm():
 
 
     def _select_parents(self) -> list[list[float]]:
-        pass
+
+        sorted_pop = sorted(self.population_and_fitness, key=lambda x: x[1], reverse=True)
+        parents = [item[0] for item in sorted_pop[:self.params.parents_number]]
+        return parents
 
 
-    def _crossing(self, chromosom_a : list, chromosom_b : list, nb_points : int = 1) -> list[float]:
-        parts = nb_points+1
+
+    def _crossing(self, chromosom_a : list, chromosom_b : list) -> list[float]:
+
+        parts = self.params.crossing_points+1
         loc = int(len(chromosom_a)/parts)
         sub_lists_a = []
         sub_lists_b = []
@@ -86,22 +87,53 @@ class GeneticAlgorithm():
             else:
                 chromosom_a.extend(sub_lists_b[i])
                 chromosom_b.extend(sub_lists_a[i])
-    
+        
+        return [chromosom_a, chromosom_b]
 
-    #TODO
-    def mutation(self, proba : float, chromosom : list) -> list:
+
+
+    def _mutation(self, chromosom : list) -> list:
+
         rand_number = random.random()
-        if rand_number < proba:
+        if rand_number < self.params.mutation_proba:
             rand_index = random.randint(0, len(chromosom)-1)
-            mutation = random.randint(-1, -3)
-            # Finir logique
+            rand_modif = random.randint(1,3)
+            rand_number = random.random()
+            if rand_number <= 0.5:
+                if chromosom[rand_index] - rand_modif >= self.params.min_phase_time:
+                    chromosom -= rand_modif
+            else:
+                chromosom += rand_modif
         
+            return self._sanitize_chromosom(chromosom)
+        return chromosom
 
+
+
+    def _update_pop(self):
+
+        parents = self._select_parents()
+        children = []
+
+        while len(children) != self.params.children_number:
+            couple = random.choices(parents, k=2)
+            children.extend(self._crossing(couple[0], couple[1]))
+
+        children = [self._mutation(child) for child in children]
+        children_and_fitness = [[child, self._compute_fitness(child)] for child in children]
         
-    def run(self, iterations : int) -> None:
+        keep_parents = len(self.population_and_fitness) - self.params.children_number
+        new_pop = sorted(self.population_and_fitness, key=lambda x: x[1], reverse=True)[:keep_parents]
+        new_pop.extend(children_and_fitness)
+        
+        self.population_and_fitness = new_pop
+
+
+
+    def run(self) -> None:
         self.srv.generate_rou_file()
-        for item in self.population:
-            self.fitness.append(self._compute_fitness(item))
+        for i in range(self.params.iterations):
+            pass
 
         
 
