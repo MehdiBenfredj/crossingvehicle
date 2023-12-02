d854a01b94e078709585d3d7676b572474368aa9
Mehdi Benfredj <mehdibenfredj3gmail.com>
2023-12-02 21:20:41 +0100

    Portage de l’algorithme génétique sur les différentes intersections avec feux ⇒ Ajouts de phases

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ from shapely import Polygon
 import os
 
 conf = GeneticConfig()
-conf.load_from_file(os.getenv("ALGCONF","config.json"))
+conf.load_from_file(os.getenv("ALGCONF","configtriple.json"))
 
 alg = GeneticAlgorithm(conf)
 
 
-#alg.run()
+alg.run()
+
+
 ####
-fitnesses = []
-for i in range(10):
-	fitnesses.append(alg.run())
-print(sum(fitnesses)/10)
+#Test changes
+#fitnesses = []
+#for i in range(10):
+#	fitnesses.append(alg.run())
+#print(sum(fitnesses)/10)
 ####
